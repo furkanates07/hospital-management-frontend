c4889a4c488763f93384cedeb0489b81c5a6c267
furkanates <furkanates4@posta.mu.edu.tr>
2024-08-28 01:07:50 +0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
@@ -6,4 +7,5 @@ import "./style.css";
 const app = createApp(App);
 
 app.use(router);
+app.use(createPinia());
 app.mount("#app");
