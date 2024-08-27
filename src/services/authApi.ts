cd73c1cf113debd313c542b4966cdd8f2dbe5b10
furkanates <furkanates4@posta.mu.edu.tr>
2024-08-28 01:10:46 +0300

    refactor: change interfaces

--- src/services/authApi.ts
+++ src/services/authApi.ts
@@ -1,16 +1,16 @@
-import { CreatePatientDto, LoginDto } from "../interfaces/index";
+import { Login, Patient } from "../interfaces/index";
 import apiClient from "./apiClient";
 
 export default {
-  loginPatient(loginData: LoginDto) {
+  loginPatient(loginData: Login) {
     return apiClient.post("/auth/patient-login", loginData);
   },
 
-  loginDoctor(loginData: LoginDto) {
+  loginDoctor(loginData: Login) {
     return apiClient.post("/auth/doctor-login", loginData);
   },
 
-  registerPatient(patientData: CreatePatientDto) {
+  registerPatient(patientData: Patient) {
     return apiClient.post("/auth/patient-register", patientData);
   },
 
