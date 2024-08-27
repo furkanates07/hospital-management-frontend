a9328414029bc8a9baaa1271a21dc6a081cf5fdc
furkanates <furkanates4@posta.mu.edu.tr>
2024-08-28 01:11:57 +0300

    update useAuth.ts

--- src/composables/useAuth.ts
+++ src/composables/useAuth.ts
@@ -1,41 +1,66 @@
+import { Role } from "@/enums/role.enum";
 import { ref } from "vue";
-import { CreatePatientDto, LoginDto } from "../interfaces/index";
+import { Login, Patient } from "../interfaces/index";
 import authApi from "../services/authApi";
 
 export function useAuth() {
   const error = ref<string | null>(null);
-  const token = ref<string | null>(null);
+  const token = ref<string | null>(localStorage.getItem("access_token"));
+  const user = ref<any>(null);
+  const userId = ref<string | null>(null);
 
-  const login = async (loginData: LoginDto, role: "doctor" | "patient") => {
+  const login = async (loginData: Login, role: Role.DOCTOR | Role.PATIENT) => {
     try {
       const response =
-        role === "doctor"
+        role === Role.DOCTOR
           ? await authApi.loginDoctor(loginData)
           : await authApi.loginPatient(loginData);
 
-      token.value = response.data.access_token;
-      console.log(token.value);
+      if (response.data.access_token) {
+        token.value = response.data.access_token;
+        localStorage.setItem("access_token", token.value ?? "");
+        userId.value = response.data.userId;
+
+        console.log("Token:", token.value);
+        console.log("User ID:", userId.value);
+      }
       error.value = null;
     } catch (err: any) {
       error.value = err.response?.data?.message || "Login failed";
     }
   };
 
-  const registerPatient = async (patientData: CreatePatientDto) => {
+  const registerPatient = async (patientData: Patient) => {
     try {
       const response = await authApi.registerPatient(patientData);
-      token.value = response.data.access_token;
-      console.log(token.value);
+      if (response.data.access_token) {
+        token.value = response.data.access_token;
+        localStorage.setItem("access_token", token.value ?? "");
+        user.value = response.data.user;
+      }
       error.value = null;
     } catch (err: any) {
       error.value = err.response?.data?.message || "Registration failed";
     }
   };
 
+  const logout = () => {
+    localStorage.removeItem("access_token");
+    token.value = null;
+    userId.value = null;
+
+    console.log("Logged out");
+    console.log("Token:", token.value);
+    console.log("User ID:", userId.value);
+  };
+
   return {
     login,
     registerPatient,
+    logout,
     error,
     token,
+    user,
+    userId,
   };
 }
