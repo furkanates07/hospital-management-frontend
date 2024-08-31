2c679045da6c300b693a78a4cc6e38bae22810e3
furkanates <furkanates4@posta.mu.edu.tr>
2024-08-31 22:25:18 +0300

    feat: add doctor and doctor update interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,5 @@
+export { DoctorUpdate } from "./doctor/doctor-update.interface";
+export { Doctor } from "./doctor/doctor.interface";
 export { EmergencyContact } from "./emergency-contact.interface";
 export { Login } from "./login.interface";
 export { PatientConditions } from "./patient/patient-conditions.interface";
