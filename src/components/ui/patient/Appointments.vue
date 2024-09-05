ac4a3cad4ec7e398387ea696cdcbdcd7426e13ae
furkanates <furkanates4@posta.mu.edu.tr>
2024-09-06 02:06:16 +0300

    feat: add button to create appointment

--- src/components/ui/patient/Appointments.vue
+++ src/components/ui/patient/Appointments.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="bg-gray-100 min-h-screen flex flex-col items-center py-4">
     <div class="w-full max-w-2xl relative">
-      <div class="flex justify-between items-center">
-        <h1 class="text-3xl font-bold text-teal-500">Appointments</h1>
+      <div class="flex flex-row justify-between">
+        <div class="flex justify-between items-center">
+          <h1 class="text-3xl font-bold text-teal-500">Appointments</h1>
+        </div>
+        <button
+          class="bg-teal-500 text-white py-2 px-4 rounded-lg shadow-md hover:bg-teal-600 transition-colors"
+        >
+          CREATE
+        </button>
       </div>
 
       <div class="py-4">
