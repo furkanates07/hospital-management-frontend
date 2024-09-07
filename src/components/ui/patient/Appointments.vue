76eca09e8faf5c2986dbeec6aff1ab048349e4c6
furkanates <furkanates4@posta.mu.edu.tr>
2024-09-07 17:14:58 +0300

    feat: add link to button

--- src/components/ui/patient/Appointments.vue
+++ src/components/ui/patient/Appointments.vue
@@ -5,11 +5,13 @@
         <div class="flex justify-between items-center">
           <h1 class="text-3xl font-bold text-teal-500">Appointments</h1>
         </div>
-        <button
-          class="bg-teal-500 text-white py-2 px-4 rounded-lg shadow-md hover:bg-teal-600 transition-colors"
+        <router-link to="/create-appointment"
+          ><button
+            class="bg-teal-500 text-white py-2 px-4 rounded-lg shadow-md hover:bg-teal-600 transition-colors"
+          >
+            CREATE
+          </button></router-link
         >
-          CREATE
-        </button>
       </div>
 
       <div class="py-4">
