2f3f5ebb8f56a6cd88b8a69a604f45f834bb6dfa
furkanates <furkanates4@posta.mu.edu.tr>
2024-09-07 17:14:41 +0300

    feat: add new path

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import AppointmentView from "../views/appointment/index.vue";
 import HomeView from "../views/home/index.vue";
 import LoginView from "../views/login/index.vue";
 import RegisterView from "../views/register/index.vue";
@@ -8,6 +9,7 @@ const routes = [
   { path: "/login", component: LoginView },
   { path: "/register", component: RegisterView },
   { path: "/home", component: HomeView },
+  { path: "/create-appointment", component: AppointmentView },
 ];
 
 const router = createRouter({
