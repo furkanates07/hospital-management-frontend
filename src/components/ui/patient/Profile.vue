5b659db73607da9bd06e1b0a0ef223b67a4f96ac
furkanates <furkanates4@posta.mu.edu.tr>
2024-09-10 12:10:22 +0300

    feat: add change password div

--- src/components/ui/patient/Profile.vue
+++ src/components/ui/patient/Profile.vue
@@ -8,11 +8,7 @@
       <div class="flex justify-end gap-3 py-3">
         <div v-if="isEditing">
           <button
-            @click="
-              () => {
-                saveChanges();
-              }
-            "
+            @click="saveChanges"
             class="bg-teal-500 text-white py-2 px-4 rounded-lg hover:bg-teal-600"
           >
             Save Changes
@@ -70,6 +66,15 @@
               <p v-if="!isEditing || !isEditableField(key)" class="mt-2 p-2">
                 {{ value || "N/A" }}
               </p>
+              <!-- Date of Birth Field -->
+              <input
+                v-else-if="key === 'Date of Birth'"
+                v-model="editableFields[key]"
+                type="date"
+                class="mt-2 p-2 border border-teal-500 rounded-lg w-full"
+                :max="currentDate"
+              />
+              <!-- Other Fields -->
               <input
                 v-else
                 v-model="editableFields[key]"
@@ -78,6 +83,76 @@
                 :placeholder="key"
               />
             </div>
+          </div>
+        </div>
+
+        <!-- Change Password Section -->
+        <div
+          class="mt-6 bg-white p-4 rounded-lg shadow-md border-l-4 border-teal-500"
+        >
+          <div v-if="!isChangingPassword" class="flex justify-center">
+            <button
+              @click="toggleChangePasswordForm"
+              class="bg-teal-500 text-white py-2 px-4 rounded-lg hover:bg-teal-600"
+            >
+              Change Password
+            </button>
+          </div>
+
+          <!-- Change Password Form -->
+          <div v-else="isChangingPassword" class="mt-4">
+            <h2 class="text-xl font-semibold text-teal-500 mb-4">
+              Change Password
+            </h2>
+            <form @submit.prevent="changePasswordHandler">
+              <div class="space-y-4">
+                <div>
+                  <input
+                    id="oldPassword"
+                    placeholder="Old Password"
+                    v-model="changePassword.oldPassword"
+                    type="password"
+                    class="mt-1 p-2 border border-teal-500 rounded-lg w-full"
+                    required
+                  />
+                </div>
+                <div>
+                  <input
+                    id="newPassword"
+                    placeholder="New Password"
+                    v-model="changePassword.newPassword"
+                    type="password"
+                    class="mt-1 p-2 border border-teal-500 rounded-lg w-full"
+                    required
+                  />
+                </div>
+              </div>
+              <div class="flex flex-col">
+                <div class="flex justify-center mt-4">
+                  <p
+                    v-if="patientStore.getError"
+                    class="mt-2 text-red-500 text-sm"
+                  >
+                    {{ patientStore.getError }}
+                  </p>
+                </div>
+                <div class="flex flex-row justify-end mt-4">
+                  <button
+                    type="submit"
+                    class="bg-teal-500 text-white py-2 px-4 rounded-lg hover:bg-teal-600"
+                  >
+                    Change Password
+                  </button>
+                  <button
+                    @click="cancelPasswordChange"
+                    type="button"
+                    class="bg-gray-300 text-black py-2 px-4 rounded-lg hover:bg-gray-400 ml-2"
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
@@ -165,7 +240,7 @@
 </template>
 
 <script setup lang="ts">
-import { EmergencyContact } from "@/interfaces";
+import { ChangePassword, EmergencyContact } from "@/interfaces";
 import { usePatientStore } from "@/stores/patient";
 import { computed, ref } from "vue";
 
@@ -174,17 +249,23 @@ interface PatientDetailsFields {
   Email: string;
   "Phone Number": string;
   "Date of Birth": string;
-  Gender: string;
   Address: string;
+  Gender: string;
 }
 
 const patientStore = usePatientStore();
 const userID = patientStore.getUserID;
 
-const formatDate = (dateString: string): string => {
-  const date = new Date(dateString);
-  return date.toLocaleDateString();
-};
+const activeTab = ref<"profile" | "contacts">("profile");
+const isEditing = ref(false);
+const isAddingNewContact = ref(false);
+const isChangingPassword = ref(false);
+const newContactName = ref("");
+const newContactPhoneNumber = ref("");
+const changePassword = ref<ChangePassword>({
+  oldPassword: "",
+  newPassword: "",
+});
 
 const patient = computed(() => patientStore.getPatient);
 
@@ -193,28 +274,35 @@ const patientDetails = computed<PatientDetailsFields>(() => ({
   Email: patient.value.email,
   "Phone Number": patient.value.phoneNumber,
   "Date of Birth": formatDate(patient.value.dateOfBirth),
-  Gender: patient.value.gender,
   Address: patient.value.address || "",
+  Gender: patient.value.gender,
 }));
 
-const isEditing = ref(false);
-const isAddingNewContact = ref(false);
-const activeTab = ref<"profile" | "contacts">("profile");
+const currentDate = computed(() => {
+  const today = new Date();
+  return today.toISOString().split("T")[0];
+});
+
+const formatDate = (dateString: string): string => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString();
+};
 
 const editableFields = ref<Partial<PatientDetailsFields>>({
   Name: patient.value.name,
   Email: patient.value.email,
   "Phone Number": patient.value.phoneNumber,
   "Date of Birth": formatDate(patient.value.dateOfBirth),
-  Gender: patient.value.gender,
   Address: patient.value.address,
 });
 
-const newContactName = ref("");
-const newContactPhoneNumber = ref("");
-
 const isEditableField = (key: keyof PatientDetailsFields): boolean => {
-  return !["Medical History", "Allergies", "Chronic Conditions"].includes(key);
+  return ![
+    "Medical History",
+    "Allergies",
+    "Chronic Conditions",
+    "Gender",
+  ].includes(key);
 };
 
 const toggleEditMode = (): void => {
@@ -224,35 +312,26 @@ const toggleEditMode = (): void => {
   }
 };
 
-const saveChanges = async (): Promise<void> => {
-  const updatedData = {
-    name: editableFields.value.Name || patient.value.name,
-    email: editableFields.value.Email || patient.value.email,
-    phoneNumber:
-      editableFields.value["Phone Number"] || patient.value.phoneNumber,
-    dateOfBirth:
-      editableFields.value["Date of Birth"] || patient.value.dateOfBirth,
-    gender: editableFields.value.Gender || patient.value.gender,
-    address: editableFields.value.Address || patient.value.address,
-    emergencyContact: patient.value.emergencyContact,
-  };
+const validatePhoneNumber = (phoneNumber: string): boolean => {
+  const phoneRegex = /^[0-9]{10}$/;
+  return phoneRegex.test(phoneNumber);
+};
 
-  console.log("User ID:", userID);
-  console.log("Updated data:", updatedData);
+const toggleChangePasswordForm = () => {
+  isChangingPassword.value = !isChangingPassword.value;
+};
 
-  if (userID) {
-    await patientStore.updatePatientDetails(userID, updatedData);
-    await patientStore.fetchPatient(userID);
-    isEditing.value = false;
+const changePasswordHandler = async (): Promise<void> => {
+  if (changePassword.value.oldPassword && changePassword.value.newPassword) {
+    await patientStore.changePassword(userID, changePassword.value);
+    changePassword.value = { oldPassword: "", newPassword: "" };
+    isChangingPassword.value = false;
   }
 };
 
-const cancelChanges = (): void => {
-  Object.keys(editableFields.value).forEach((key) => {
-    const fieldKey = key as keyof PatientDetailsFields;
-    editableFields.value[fieldKey] = patientDetails.value[fieldKey];
-  });
-  patientStore.fetchPatient(userID);
+const cancelPasswordChange = (): void => {
+  changePassword.value = { oldPassword: "", newPassword: "" };
+  isChangingPassword.value = false;
 };
 
 const startAddingNewContact = (): void => {
@@ -267,6 +346,11 @@ const cancelNewContact = (): void => {
 
 const submitNewContact = async (): Promise<void> => {
   if (newContactName.value && newContactPhoneNumber.value) {
+    if (!validatePhoneNumber(newContactPhoneNumber.value)) {
+      alert("Invalid phone number format. Please enter a valid phone number.");
+      return;
+    }
+
     const newContact: EmergencyContact = {
       name: newContactName.value,
       phoneNumber: newContactPhoneNumber.value,
@@ -290,6 +374,42 @@ const deleteContact = (index: number): void => {
   if (confirm("Are you sure you want to delete this contact?")) {
     patient.value.emergencyContact?.splice(index, 1);
   }
+};
+
+const saveChanges = async (): Promise<void> => {
+  if (
+    editableFields.value["Phone Number"] &&
+    !validatePhoneNumber(editableFields.value["Phone Number"])
+  ) {
+    alert("Invalid phone number format. Please enter a valid phone number.");
+    return;
+  }
+
+  const updatedData = {
+    name: editableFields.value.Name || patient.value.name,
+    email: editableFields.value.Email || patient.value.email,
+    phoneNumber:
+      editableFields.value["Phone Number"] || patient.value.phoneNumber,
+    dateOfBirth:
+      editableFields.value["Date of Birth"] || patient.value.dateOfBirth,
+    gender: patient.value.gender,
+    address: editableFields.value.Address || patient.value.address,
+    emergencyContact: patient.value.emergencyContact,
+  };
+
+  if (userID) {
+    await patientStore.updatePatientDetails(userID, updatedData);
+    await patientStore.fetchPatient(userID);
+    isEditing.value = false;
+  }
+};
+
+const cancelChanges = (): void => {
+  Object.keys(editableFields.value).forEach((key) => {
+    const fieldKey = key as keyof PatientDetailsFields;
+    editableFields.value[fieldKey] = patientDetails.value[fieldKey];
+  });
+  patientStore.fetchPatient(userID);
 };
 </script>
 
