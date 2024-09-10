82b1b70af3217984eadc16628484676a5ecb197d
furkanates <furkanates4@posta.mu.edu.tr>
2024-09-10 18:49:19 +0300

    feat: create examination view for doctor

---
+++ src/views/examination/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>examination</div>
+</template>
+
+<script setup lang="ts">
+import { Doctor, Patient } from "@/interfaces";
+import { Appointment } from "@/interfaces/appointment";
+import { useAppointmentStore } from "@/stores/appointment";
+import { useDoctorStore } from "@/stores/doctor";
+import { usePatientStore } from "@/stores/patient";
+import { onMounted, ref } from "vue";
+
+const doctorStore = useDoctorStore();
+const appointmentStore = useAppointmentStore();
+const patientStore = usePatientStore();
+
+const appointment = ref<Appointment | null>(null);
+const patient = ref<Patient | null>(null);
+const doctor = ref<Doctor | null>(null);
+
+onMounted(async () => {
+  appointment.value = await appointmentStore.getAppointment;
+  patient.value = await patientStore.getPatient;
+  doctor.value = await doctorStore.getDoctor;
+  console.log(appointment.value);
+  console.log(patient.value);
+  console.log(doctor.value);
+});
+</script>
+
+<style scoped></style>
