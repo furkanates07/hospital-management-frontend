1e9a99abb5a24b08e42917bf1bb56fa9a258102e
furkanates <furkanates4@posta.mu.edu.tr>
2024-09-10 12:02:24 +0300

    feat: Home ui design

--- src/components/ui/patient/Home.vue
+++ src/components/ui/patient/Home.vue
@@ -1,7 +1,59 @@
 <template>
-  <div>Home</div>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div class="bg-white p-16 rounded-lg shadow-lg max-w-md text-center">
+      <h1 class="text-3xl font-bold text-teal-500 mb-4">
+        Welcome, {{ patientName }}!
+      </h1>
+
+      <div class="my-6 space-y-4">
+        <p class="text-lg text-gray-700">
+          You have
+          <span class="font-bold text-teal-500">
+            {{ pendingAppointments.length }}
+          </span>
+          pending appointments.
+        </p>
+        <p class="text-lg text-gray-700">
+          You have
+          <span class="font-bold text-teal-500">
+            {{ approvedAppointments.length }}
+          </span>
+          approved appointments.
+        </p>
+      </div>
+
+      <div class="mt-8">
+        <p class="text-gray-600">
+          Use the menu on the left to view more details about your appointments.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Status } from "@/enums/status.enum";
+import { Appointment } from "@/interfaces/appointment";
+import { usePatientStore } from "@/stores/patient";
+import { onMounted, ref } from "vue";
+
+const patientStore = usePatientStore();
+
+const pendingAppointments = ref<Appointment[]>([]);
+const approvedAppointments = ref<Appointment[]>([]);
+const patientName = ref<string>("");
+
+onMounted(async () => {
+  await patientStore.fetchAppointments(patientStore.userID);
+  patientName.value = patientStore.getPatientFullName;
+
+  pendingAppointments.value = patientStore.getAppointments.filter(
+    (appointment) => appointment.status === Status.PENDING
+  );
+  approvedAppointments.value = patientStore.getAppointments.filter(
+    (appointment) => appointment.status === Status.APPROVED
+  );
+});
+</script>
 
 <style scoped></style>
